d56b9cc569bc6269a32623fcb6c4a61bb050534b
Tolga BAYRAK <bayraktolga28@gmail.com>
2023-03-10 13:20:51 +0300

    redux-toolkit

--- client-v1/src/main.tsx
+++ client-v1/src/main.tsx
@@ -4,14 +4,17 @@ import { BrowserRouter } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
 import './index.css'
-
+import { store } from "./app/store"
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
+      <Provider store={store}>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 )
